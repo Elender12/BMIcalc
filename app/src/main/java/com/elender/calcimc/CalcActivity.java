5df21560e0f23fb46594727728b2dd24beb2872a
Elen <elender1230@gmail.com>
2020-12-04 22:14:26 +0100

    master: show BMI clasification, add new items in colors.xml

--- app/src/main/java/com/elender/calcimc/CalcActivity.java
+++ app/src/main/java/com/elender/calcimc/CalcActivity.java
@@ -6,14 +6,18 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class CalcActivity extends AppCompatActivity {
+    private static final String TAG = "CalcActivity";
     EditText name, height, weight, age;
     RadioGroup radioGroup;
     Button btn;
+
     /**
      * @param savedInstanceState
      */
@@ -22,23 +26,69 @@ public class CalcActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
         ConstraintLayout constraintLayout = findViewById(R.id.calcLayout);
-       //variables para el cambio de color de fondo
+        //variables para el cambio de color de fondo
         AnimationDrawable bckDrawable = (AnimationDrawable) constraintLayout.getBackground();
         bckDrawable.setEnterFadeDuration(2000);
         bckDrawable.setExitFadeDuration(4000);
         bckDrawable.start();
         //unir las variables con los elementos del layout
         name = findViewById(R.id.ET_name);
-        height= findViewById(R.id.ET_height);
+        height = findViewById(R.id.ET_height);
         weight = findViewById(R.id.ET_weight);
         age = findViewById(R.id.ET_age);
         radioGroup = findViewById(R.id.radioGroupGender);
         btn = findViewById(R.id.btnCalc);
         btn.setOnClickListener(v -> {
+            float resultsBmi = calculateBMI();
+            double [] resultsFCMax = calculateFCMax();
             Intent intent = new Intent(this, ResultsActivity.class);
-            //intent.put extra...
+            intent.putExtra("bmi", resultsBmi);
+            intent.putExtra("fcmax", resultsFCMax);
             startActivity(intent);
         });
     }
+    private float calculateBMI() {
+        float result = 0;
+        try {
+            float weightValue = Float.parseFloat(String.valueOf(weight.getText()));
+            float heightValue = Float.parseFloat(String.valueOf(height.getText()));
+            result = (float) (weightValue / Math.pow(heightValue, 2));
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(this, "Has introducido unos valores incorrectos.", Toast.LENGTH_LONG);
+        }
+        return result;
+    }
 
+    /**
+     * El metodo calcula la frecuencia cardiaca maxima
+     * @return double [] resultados
+     * La primera posicion almacena el valor para las personas sedentarias
+     * La segunda posicion almacena el valor para las personas entrenadas
+     */
+    private double[] calculateFCMax() {
+        //primera posición valores para personas sedentarias, segunda posicion p
+        double[] results = new double[2];
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioFemale) {
+                    //Mujeres entrenadas: FCmax = 214 – (0,8 x edad)
+                    //Mujeres sedentarias: 225 - edad
+                    double sedentaryFCMax = 225 - Integer.parseInt(String.valueOf(age.getText()));
+                    double trainedFCMax = 214 - (0.8 *  (Integer.parseInt(String.valueOf(age.getText()))));
+                    results[0] = sedentaryFCMax;
+                    results[1] = trainedFCMax;
+                } else if (checkedId == R.id.radioMale) {
+                    //Hombres: FCmax = 209 – (0,7 x edad)
+                    double sedentaryFCMax = 220 - Integer.parseInt(String.valueOf(age.getText()));
+                    double trainedFCMax = 209 - (0.7 * (Integer.parseInt(String.valueOf(age.getText()))));
+                    results[0] = sedentaryFCMax;
+                    results[1] = trainedFCMax;
+                }
+            }
+        });
+        return results;
+    }
 }
+
--- app/src/main/java/com/elender/calcimc/ResultsActivity.java
+++ app/src/main/java/com/elender/calcimc/ResultsActivity.java
@@ -3,18 +3,77 @@ package com.elender.calcimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 public class ResultsActivity extends AppCompatActivity {
-    TextView results;
+    TextView results, bmiTV, bmiClasification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Bundle bundle = getIntent().getExtras();
+        //recojo los datos desde la actividad anterior
+        float bmiValue = 0;
+        double [] fcMaxValues = new double[2];
+        if (bundle != null) {
+            bmiValue = bundle.getFloat("bmi");
+            fcMaxValues = bundle.getDoubleArray("fcmax");
+        }
         results = findViewById(R.id.textViewResults);
+        bmiTV = findViewById(R.id.TV_bmiValue);
+        bmiClasification = findViewById(R.id.TV_clasification);
+
         ObjectAnimator animation = ObjectAnimator.ofFloat(results, "scaleX", 2);
         animation.setDuration(4000);
         animation.start();
+        showResults(bmiValue, fcMaxValues);
+    }
+
+    /**
+     * @param bmiValue
+     * @param fcMaxValue
+     */
+    private void showResults(float bmiValue, double [] fcMaxValue){
+        if(bmiValue < 18.5){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.lowWeight));
+            bmiClasification.setTextColor(Color.YELLOW);
+        }else if (bmiValue >= 18.5 && bmiValue <= 24.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.normalWeight));
+            bmiClasification.setTextColor(Color.GREEN);
+        }else if(bmiValue >= 25 && bmiValue <= 26.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.overweightI));
+            bmiClasification.setTextColor(getColor(R.color.warningI));
+        }else if(bmiValue >= 27 && bmiValue <= 29.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.overWeightII));
+            bmiClasification.setTextColor(getColor(R.color.warningII));
+        }else if(bmiValue >= 30 && bmiValue <=34.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.obesityI));
+            bmiClasification.setTextColor(getColor(R.color.severeWarning));
+        }else if(bmiValue >= 35 && bmiValue <= 39.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.obesityII));
+            bmiClasification.setTextColor(getColor(R.color.severeWarningI));
+        }else if(bmiValue >= 40 && bmiValue <= 49.9){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.obesityIII));
+            bmiClasification.setTextColor(Color.RED);
+        }else if(bmiValue > 50){
+            bmiTV.setText(String.format(Locale.getDefault(), "%s  %.2f%n", getString(R.string.bmi), bmiValue));
+            bmiClasification.setText(getString(R.string.obesityIV));
+            bmiClasification.setTextColor(getColor(R.color.extremeWarning));
+        }
+
+
+
     }
 }
